ef2b5ed2eb6cf1c65f0206351ad5f92481a104e0
ColdsteelRail <574252631@qq.com>
2020-06-19 16:13:45 +0800

    添加完所有实体类

---
+++ src/com/web/entity/Favor.java
@@ -0,0 +1,19 @@
+package com.web.entity;
+
+public class Favor {
+	private Integer CID;
+	private Integer LID;
+	
+	public Integer getCID() {
+		return CID;
+	}
+	public void setCID(Integer cID) {
+		CID = cID;
+	}
+	public Integer getLID() {
+		return LID;
+	}
+	public void setLID(Integer lID) {
+		LID = lID;
+	}
+}
---
+++ src/com/web/entity/Order.java
@@ -0,0 +1,69 @@
+package com.web.entity;
+import java.math.*;
+
+public class Order {
+	private Integer CID;
+	private Integer LID;
+	private String  OID;
+	private Integer NUM;
+	private String  DATE;
+	private String  PAY;
+	private BigDecimal SUMPRICE;
+	private Integer STATE;
+	private String COMMENT;
+	
+	public Integer getCID() {
+		return CID;
+	}
+	public void setCID(Integer cID) {
+		CID = cID;
+	}
+	public Integer getLID() {
+		return LID;
+	}
+	public void setLID(Integer lID) {
+		LID = lID;
+	}
+	public String getOID() {
+		return OID;
+	}
+	public void setOID(String oID) {
+		OID = oID;
+	}
+	public Integer getNUM() {
+		return NUM;
+	}
+	public void setNUM(Integer nUM) {
+		NUM = nUM;
+	}
+	public String getDATE() {
+		return DATE;
+	}
+	public void setDATE(String dATE) {
+		DATE = dATE;
+	}
+	public String getPAY() {
+		return PAY;
+	}
+	public void setPAY(String pAY) {
+		PAY = pAY;
+	}
+	public BigDecimal getSUMPRICE() {
+		return SUMPRICE;
+	}
+	public void setSUMPRICE(BigDecimal sUMPRICE) {
+		SUMPRICE = sUMPRICE;
+	}
+	public Integer getSTATE() {
+		return STATE;
+	}
+	public void setSTATE(Integer sTATE) {
+		STATE = sTATE;
+	}
+	public String getCOMMENT() {
+		return COMMENT;
+	}
+	public void setCOMMENT(String cOMMENT) {
+		COMMENT = cOMMENT;
+	}
+}
